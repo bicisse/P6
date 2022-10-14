edf64849d66c366c3436bf8872e1420f715f07d6
bicisse <105852207+bicisse@users.noreply.github.com>
2022-10-14 10:35:46 +0200

    tests rrouter

--- backend/routes/sauces.js
+++ backend/routes/sauces.js
@@ -1,40 +1,34 @@
 const express= require('express');
 const router = express.Router();
 
-
-router.get('/', (req, res)=> {
-    console.log('Test GET /');
-    res.send('Test GET /');
-})
-
-// router.post('/', (req, res)=>{
-//     console.log('Test POST /sauces');
-//     res.render('Test post /sauces')
-// })
-
-router.post('/', (req, res) =>{
-    console.log('test POST /');
-    res.send('test POST /');
-});
+router.route('/')
+    .get((req, res)=> {
+            console.log('Test GET SAUCES /');
+            res.send('Test GET SAUCES /');
+        })
+    .post((req, res) =>{
+            console.log('test POST SAUCES /');
+            res.send('test POST SAUCES/');
+        });
 
 router.route('/:id')
-.get((req,res) =>{
-    console.log(req.user);
-    res.send(`Get User With ID ${req.params.id}`)
-})
-.put((req,res) =>{
-    req.params.id
-    res.send(`Update User With ID ${req.params.id}`)
-})
-.delete((req,res) =>{
-    req.params.id
-    res.send(`Delete User With ID ${req.params.id}`)
-});
+    .get((req,res) =>{
+        res.send('test GET SAUCES ID ');
+    })
+    .put((req,res) =>{
+        req.params.id
+        res.send('test PUT SAUCES ID ');
+    })
+    .delete((req,res) =>{
+        req.params.id
+        res.send('test DELETE SAUCES ID ');
+    });
 
-const users = [{name : 'Harry'}, {name : 'Hermione'}];
-router.param('id', (req, res, next, id)=>{
-    req.user = users[id]
-    next()
+router.post("/:id/like", (req, res)=>{
+    console.log('test POST /');
+     res.send('test POST SAUCES ID LIKE ');
 })
+
+
 
 module.exports = router
