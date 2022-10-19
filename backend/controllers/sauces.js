d7903fbe886e7a0db2c33c5a0363c369ba2d3bb0
bicisse <105852207+bicisse@users.noreply.github.com>
2022-10-19 14:04:11 +0200

    bouton like

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -154,127 +154,85 @@ exports.likeSauce = (req, res, next) => {
             _id: req.params.id
         })
         .then((sauce) => {
-            console.log(sauce.usersLiked);
+
+            console.log('UsersLiked', sauce.usersLiked);
+            console.log('UsersDisliked', sauce.usersDisliked);
             const userId = req.body.userId;
             const previousLikeVote = sauce.usersLiked.findIndex(element => element == userId);
             const previousDislikeVote = sauce.usersDisliked.findIndex(element => element == userId);
-            console.log(req.body.like);
+            console.log('REQ BODY LIKE', req.body.like);
+            console.log('TEST TEST ', sauce.dislike);
             const thumb = req.body.like;
             //
-            function incLike() {
-                Sauces.updateOne({
-                        _id: req.params.id
-                    }, {
-                        $inc: {
-                            like: +1
-                        },
-                        $push: {
-                            usersLiked: userId
-                        }
-                    })
-                    .then(() => res.status(200).json({
-                        message: 'UPDATED!'
-                    }))
-                    .catch(error => res.status(401).json({
-                        error: error
-                    }));
-            }
-
-
-            function incDislike() {
-                Sauces.updateOne({
-                        _id: req.params.id
-                    }, {
-                        $inc: {
-                            dislike: +1
-                        },
-                        $push: {
-                            usersDisliked: userId
-                        }
-                    })
-                    .then(() => res.status(200).json({
-                        message: 'Dislike increase'
-                    }))
-                    .catch(error => res.status(401).json({
-                        error: error
-                    }));
-            }
-
-            function decLike() {
-                Sauces.updateOne({
-                        _id: req.params.id
-                    }, {
-                        $inc: {
-                            like: -1
-                        },
-                        $pull: {
-                            usersLiked: userId
-                        }
-                    })
-                    .then(() => res.status(200).json({
-                        message: 'Liked cancelled'
-                    }))
-                    .catch(error => res.status(401).json({
-                        error: error
-                    }));
-            }
-
-            function decDislike() {
-                Sauces.updateOne(
-                  {
-                        _id: req.params.id
-                    }, {
-                        $inc: {
-                            dislike: -1
-                        },
-                        $pull: {
-                            usersDisliked: userId
-                        }
-                    })
-                    .then(() => res.status(200).json({
-                        message: 'Disliked cancelled'
-                    }))
-                    .catch(error => res.status(401).json({
-                        error: error
-                    }));
-            }
-
-function top(XXX){
-  Sauces.updateOne( 
-    XXX
-     )
-.then(() => res.status(200).json({
-    message: 'Disliked cancelled'
-}))
-.catch(error => res.status(401).json({
-    error: error
-}));
-}
-
-
-            console.log('NEWWWWWW');
+            console.log('------');
 
             switch (thumb) {
                 // ALREADY VOTED
                 case 0:
                     if (previousDislikeVote !== -1) {
                         console.log("Don't dislike anymore");
-                        decDislike()
-                    } else if (previousLikeVote == !-1) {
+                        Sauces.updateOne(
+                          { _id: req.params.id},
+                          {$inc: {dislikes: -1}, 
+                       $pull: {usersDisliked: userId}
+                      }
+                        )
+                            .then(() => res.status(200).json({
+                                message: 'Dislike decreased'
+                            }))
+                            .catch(error => res.status(401).json({
+                                error
+                            }));
+                    } else if (previousLikeVote !== -1) {
                         console.log("Don't like anymore");
-                        decLike()
+                        Sauces.updateOne(
+                          { _id: req.params.id},
+                          {$inc: {likes: -1}, 
+                       $pull: {usersLiked: userId}
+                      }
+                        )
+                            .then(() => res.status(200).json({
+                                message: 'Like decreased'
+                            }))
+                            .catch(error => res.status(401).json({
+                                error
+                            }));
                     }
                     break;
                     //LIKE
                 case 1:
                     console.log('Like');
-                    incLike()
-                    break;
+                    Sauces.updateOne(
+                      { _id: req.params.id},
+                      {$inc: {dislikes: +1}, 
+                   $push: {usersDisliked: userId}
+                  }
+                    )
+                    .then(() => res.status(200).json({
+                        message: 'Like increased'
+                    }))
+                    .catch(error => res.status(401).json({
+                        error
+                    }));
+                      break;
 
                     //DISLIKE
                 case -1:
                     console.log('dislike');
-                    incDislike()
+                    Sauces.updateOne(
+                      { _id: req.params.id},
+                      {
+                        $inc: {dislikes: +1}, 
+                   $push: {
+                          usersDisliked: userId
+                      }
+                  })
+                  .then(() => res.status(200).json({
+                      message: 'Dislike increased'
+                  }))
+                  .catch(error => res.status(401).json({
+                      error
+                  }));
                     break;
                 default:
                     'Unexpected error'
@@ -282,8 +240,9 @@ function top(XXX){
             }
 
             console.log(req.body.like);
-            console.log('PD', previousDislikeVote);
-            console.log('PL', previousLikeVote);
+            console.log('UsersLiked', sauce.usersLiked);
+            console.log('UsersDisliked', sauce.usersDisliked);
+
         })
         .catch(error => {
             res.status(500).json({
